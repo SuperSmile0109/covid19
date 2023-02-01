29ed1452f8fd9447e39c5e9311299ecbff39751d
SuperSmile0109 <94983960+SuperSmile0109@users.noreply.github.com>
2023-02-01 06:54:06 +0000

    Update

--- src/App.js
+++ src/App.js
@@ -1,36 +1,8 @@
 import React from 'react';
-import { Cards, Charts, CountryPicker } from './components';
 import { fetchData } from "./api";
-import styles from './App.module.css';
-import coronaImage from './images/image.png';
-import Particles from "react-particles-js";
-import BottomNavigationAction from "@material-ui/core/BottomNavigationAction";
-import BottomNavigation from "@material-ui/core/BottomNavigation";
-import GitHubIcon from '@material-ui/icons/GitHub';
-import { Typography} from "@material-ui/core";
-import Link from "@material-ui/core/Link";
 
 
 class App extends React.Component{
-    state ={
-        data: {},
-        country:'',
-    }
-
-    async componentDidMount() {
-        const fetchedData = await fetchData();
-
-        this.setState({ data: fetchedData });
-    }
-
-    handleCountryChange = async (country) => {
-        const fetchedData = await fetchData(country);
-        this.setState({ data: fetchedData, country:country });
-
-
-    }
-    handleChange;
-    handleClick;
     render() {
         const {data, country} = this.state;
             return (
