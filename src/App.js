afdef0938de6e31753b1bb8fb2df02102f2ead79
SuperSmile0109 <94983960+SuperSmile0109@users.noreply.github.com>
2023-02-01 08:20:40 +0000

    delete

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import React from 'react';
 
 class App extends React.Component{
     render() {
-        const {data, country} = this.state;
             return (
                 <div style={{backgroundColor: 'white'}}>
                  Hello
